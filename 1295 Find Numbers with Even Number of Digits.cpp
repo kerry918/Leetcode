ddfdf0397eed4222ba5f9ced23ab36a83b2a682f
Kerry Liu <56353549+kerry918@users.noreply.github.com>
2020-08-20 12:04:52 -0400

    Create 1295 Find Numbers with Even Number of Digits.cpp

---
+++ 1295 Find Numbers with Even Number of Digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int ans = 0; 
+        for (int i = 0; i < nums.size(); i++){
+            int digit = 0; 
+            while (nums[i] > 0){
+                nums[i] /= 10; 
+                digit ++; 
+            }
+            if (digit%2 == 0){
+                ans ++; 
+            }
+        }
+        return ans; 
+    }
+};
